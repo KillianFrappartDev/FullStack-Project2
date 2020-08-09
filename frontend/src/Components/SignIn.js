b29fabc289f4450866b8bc12d0973f1e7de3d5d0
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-08-09 12:33:50 +0100

    Input validation

--- frontend/src/Components/SignIn.js
+++ frontend/src/Components/SignIn.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+import React, { useReducer } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='https://material-ui.com/'>
         Killian Frappart
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
@@ -29,16 +29,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.primary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -46,125 +46,145 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn(props) {
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState(false);
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
-  const [disabled, setDisabled] = useState(true);
-  const [remember, setRemember] = useState(false);
-  const classes = useStyles();
+const initialState = {
+  email: {
+    value: '',
+    isValid: true,
+  },
+  password: {
+    value: '',
+    isValid: true,
+  },
+  disabled: true,
+  remember: false,
+};
 
-  const emailHandler = (event) => {
-    const value = event.target.value;
-    const reg = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-    setEmail(value);
-    if (!reg.test(value)) {
-      setEmailError(true);
+const reducer = (state = initialState, action) => {
+  const reg = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+  const check = (input) => {
+    if (input.value.trim().length > 0 && input.isValid) {
+      return true;
     } else {
-      setEmailError(false);
-      disableCheck();
+      return false;
     }
-    return;
   };
 
-  const passwordHandler = (event) => {
-    const value = event.target.value;
-    setPassword(value);
-    if (value.length < 6) {
-      setPasswordError(true);
-    } else {
-      setPasswordError(false);
-      disableCheck();
-    }
-    return;
-  };
-
-  const rememberHandler = () => {
-    remember ? setRemember(false) : setRemember(true);
+  switch (action.type) {
+    case 'email':
+      if (reg.test(action.value) && check(state.password)) {
+        return { ...state, email: { value: action.value, isValid: true }, disabled: false };
+      } else if (reg.test(action.value)) {
+        return { ...state, email: { value: action.value, isValid: true }, disabled: true };
+      } else {
+        return { ...state, email: { value: action.value, isValid: false }, disabled: true };
+      }
+    case 'password':
+      if (action.value.trim().length > 5 && check(state.email)) {
+        return { ...state, password: { value: action.value, isValid: true }, disabled: false };
+      } else if (action.value.trim().length > 5) {
+        return { ...state, password: { value: action.value, isValid: true }, disabled: true };
+      } else {
+        return { ...state, password: { value: action.value, isValid: false }, disabled: true };
+      }
+    case 'remember':
+      if (state.remember) {
+        return { ...state, remember: false };
+      } else {
+        return { ...state, remember: true };
+      }
+    default:
+      return { ...state };
   }
+};
 
-  const disableCheck = () => {
-    if (email.trim().length > 0 && password.trim().length > 0) {
-      emailError || passwordError ? setDisabled(true) : setDisabled(false);
-      return;
-    }
-
-    return;
-  };
+export default function SignIn(props) {
+  const classes = useStyles();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(`Login: ${email} / ${password} / ${remember}`);
+    console.log(`Login: ${state.email.value} / ${state.password.value} / ${state.remember}`);
     return;
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component='main' maxWidth='xs'>
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component='h1' variant='h5'>
           Log in
         </Typography>
         <form onSubmit={submitHandler} className={classes.form} noValidate>
           <TextField
-            className="inputs-bg"
-            variant="outlined"
-            margin="normal"
+            className='inputs-bg'
+            variant='outlined'
+            margin='normal'
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
             autoFocus
-            color="primary"
-            onChange={emailHandler}
-            value={email}
-            error={emailError}
-            helperText={emailError ? "Email is not valid." : ""}
+            color='primary'
+            onChange={(e) => {
+              dispatch({ type: e.target.id, value: e.target.value });
+            }}
+            value={state.email.value}
+            error={!state.email.isValid}
+            helperText={!state.email.isValid ? 'Email is not valid.' : ''}
           />
           <TextField
-            variant="outlined"
-            margin="normal"
+            variant='outlined'
+            margin='normal'
             required
             fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            color="primary"
-            onChange={passwordHandler}
-            value={password}
-            error={passwordError}
-            helperText={passwordError ? "Password must contain at least 6 characters." : ""}
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+            color='primary'
+            onChange={(e) => {
+              dispatch({ type: e.target.id, value: e.target.value });
+            }}
+            value={state.password.value}
+            error={!state.password.isValid}
+            helperText={!state.password.isValid ? 'Email is not valid.' : ''}
           />
           <FormControlLabel
-            control={<Checkbox onChange={rememberHandler} value={remember} color="primary" />}
-            label="Remember me"
+            control={
+              <Checkbox
+                value={state.remember}
+                onChange={(e) => {
+                  dispatch({ type: 'remember' });
+                }}
+                color='primary'
+              />
+            }
+            label='Remember me'
           />
           <Button
-            type="submit"
+            type='submit'
             fullWidth
-            variant="contained"
-            color="secondary"
-            className={classes.submit}
-            disabled={disabled}
-          >
+            variant='contained'
+            color='secondary'
+            disabled={state.disabled}
+            className={classes.submit}>
             Sign In
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href='#' variant='body2'>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link onClick={props.switch} href="#" variant="body2">
+              <Link onClick={props.switch} href='#' variant='body2'>
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
--- frontend/src/Pages/AuthPage.js
+++ frontend/src/Pages/AuthPage.js
@@ -4,16 +4,13 @@ import SignIn from '../Components/SignIn';
 import SignUp from '../Components/SignUp';
 
 const AuthPage = () => {
-    const [loginMode, setLoginMode] = useState(true);
+  const [loginMode, setLoginMode] = useState(true);
 
-    const switchHandler = () => {
-        loginMode ? setLoginMode(false) : setLoginMode(true);
-    }
+  const switchHandler = () => {
+    loginMode ? setLoginMode(false) : setLoginMode(true);
+  };
 
-    return (
-        loginMode ? <SignIn switch={switchHandler} /> : <SignUp switch={switchHandler} />
-    );
-}
+  return loginMode ? <SignIn switch={switchHandler} /> : <SignUp switch={switchHandler} />;
+};
 
 export default AuthPage;
-
